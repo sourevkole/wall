cea33e3cf6555e8eed3cf7d27559a1e1c29e986a
souravkole <sourev.kole87@gmail.com>
2020-02-03 03:28:32 +0800

    Added Reactive Form

---
+++ src/app/interface/sign-up-details.ts
@@ -0,0 +1,5 @@
+export interface SignUpDetails {
+    email: string;
+    fullName: string;
+    password: string;
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication-service.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
-  constructor(public authService:AuthenticationService) { }
+  signUpFormGroup: FormGroup;
+  signUpSubmitted:boolean = false;
+
+  constructor(private authService:AuthenticationService, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.signUpFormGroup = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmpassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get formControls(){
+    return this.signUpFormGroup.controls;
+  }
+
+  signUp() {
+    this.signUpSubmitted = true;
+    if(this.signUpFormGroup.invalid){
+      return;
+    }else{
+      alert('Success');
+    }
+  }
 
   googleSignIn() {
     this.authService.googleAuth();
